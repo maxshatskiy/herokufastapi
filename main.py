137fb3ee8ee87dd3866f89d8275224b84bfd53ca
maxmustermannde <shatskiymaxim@gmail.com>
2022-10-23 21:50:10 +0200

    changed main

--- main.py
+++ main.py
@@ -94,6 +94,6 @@ def model_inference(data:Person):
     return {"prediction": preds.tolist()[0]}
 
 if __name__=="__main__":
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app)
 
 
